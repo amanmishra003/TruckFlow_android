86722988caa58c67256dbcef314b8264193d5898
drake7 <Deepak.kumar7v@gmail.com>
2023-07-26 21:54:29 -0700

    Changes for trucker shipper user management

---
+++ app/src/main/java/com/example/truckflow/trucker/TruckerDetails.java
@@ -0,0 +1,16 @@
+package com.example.truckflow.trucker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.truckflow.R;
+
+public class TruckerDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trucker_details);
+    }
+}
