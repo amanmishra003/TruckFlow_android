8bda079e6507adb271469fd43ceb44be83b61507
Deepak Kumar <deepak.kumar7v@gmail.com>
2023-08-09 09:27:20 -0700

    Changes for booking action

--- app/src/main/java/com/example/truckflow/booking/BookingRequests.java
+++ app/src/main/java/com/example/truckflow/booking/BookingRequests.java
@@ -3,9 +3,14 @@ package com.example.truckflow.booking;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,6 +30,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -41,6 +48,8 @@ public class BookingRequests extends AppCompatActivity {
 
     DatabaseReference databaseRef;
     private User userDetail;
+
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,55 +69,79 @@ public class BookingRequests extends AppCompatActivity {
             public void onAcceptClick(BookingRequest bookingRequest) {
                 UUID uuid = UUID.randomUUID();
                 //if user press on accept,then we will send the confirmation to the trucker
-                databaseRef.child("bookings").child(String.valueOf(uuid)).setValue(bookingRequest)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Log.d(TAG, "booking data saved to Firebase Realtime Database");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.e(TAG, "Error saving booking data to Firebase Realtime Database", e);
-                            }
-                        });
+                databaseRef.child("bookings").child(String.valueOf(uuid)).setValue(bookingRequest).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, "booking data saved to Firebase Realtime Database");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Error saving booking data to Firebase Realtime Database", e);
+                    }
+                });
 
-                db.collection("bookings")
-                        .add(bookingRequest)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "Trucker data saved to Firestore with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.e(TAG, "Error saving trucker data to Firestore", e);
-                            }
-                        });
+                db.collection("bookings").add(bookingRequest).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "Trucker data saved to Firestore with ID: " + documentReference.getId());
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Error saving trucker data to Firestore", e);
+                    }
+                });
 
                 FireBaseUtils.getUserDetails(bookingRequest.getTruckerEmail(), new FireBaseUtils.FirestoreUserCallback() {
                     @Override
                     public void onUserReceived(User user) {
                         if (user != null) {
                             userDetail = user;
-                            String token =  userDetail.token;
-                            FireBaseUtils.sendNotification("TruckFlow","Your booking requeest is accepted",token,BookingRequests.this);
+                            String token = userDetail.token;
+                            FireBaseUtils.sendNotification("TruckFlow", "Your booking requeest is accepted", token, BookingRequests.this);
 
                         } else {
                             // Handle the case where trucker details were not found
                         }
                     }
                 });
+
+                showCustomDialog("You have Accepted this booking");
                 Toast.makeText(BookingRequests.this, "Accepted: " + bookingRequest.getLoadName(), Toast.LENGTH_SHORT).show();
 
             }
 
             @Override
             public void onRejectClick(BookingRequest bookingRequest) {
-                // Handle reject button click
+                Query query = db.collection("bookingRequest").whereEqualTo("shipperId", bookingRequest.getShipperId()).whereEqualTo("truckerEmail", bookingRequest.getTruckerEmail());
+                query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                document.getReference().delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "Document deleted successfully");
+                                        showCustomDialog("You have rejected this booking");
+                                        recreate();
+                                        adapter.notifyDataSetChanged();
+
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e(TAG, "Error deleting document", e);
+                                    }
+                                });
+                            }
+                        } else {
+                            Log.e(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
                 Toast.makeText(BookingRequests.this, "Rejected: " + bookingRequest.getLoadName(), Toast.LENGTH_SHORT).show();
             }
 
@@ -117,7 +150,7 @@ public class BookingRequests extends AppCompatActivity {
                 // Handle chat button click
                 String recipientId = bookingRequest.getTruckerEmail();
                 Intent i = new Intent(BookingRequests.this, ChatActivity.class);
-                i.putExtra("receiverId",recipientId);
+                i.putExtra("receiverId", recipientId);
                 startActivity(i);
                 Toast.makeText(BookingRequests.this, "Chat: " + bookingRequest.getTruckerEmail(), Toast.LENGTH_SHORT).show();
             }
@@ -125,23 +158,45 @@ public class BookingRequests extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         // Fetch booking requests from Firebase Firestore
-        db.collection("bookingRequest")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (DocumentSnapshot document : task.getResult()) {
-                                BookingRequest bookingRequest = document.toObject(BookingRequest.class);
-                                if (bookingRequest != null) {
-                                    bookingRequests.add(bookingRequest);
-                                }
-                            }
-                            adapter.notifyDataSetChanged();
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
+        db.collection("bookingRequest").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult()) {
+                        BookingRequest bookingRequest = document.toObject(BookingRequest.class);
+                        if (bookingRequest != null) {
+                            bookingRequests.add(bookingRequest);
                         }
                     }
-                });
+                    adapter.notifyDataSetChanged();
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void showCustomDialog(String message) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialoug_layout, null);
+        dialogBuilder.setView(dialogView);
+
+        final TextView dialogTitle = dialogView.findViewById(R.id.dialogTitle);
+        final Button dialogButton = dialogView.findViewById(R.id.dialogButton);
+
+        dialogTitle.setText(message);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the input data or perform actions
+
+                // Dismiss the dialog
+                alertDialog.dismiss();
+            }
+        });
+
+        alertDialog = dialogBuilder.create();
+        alertDialog.show();
     }
 }
